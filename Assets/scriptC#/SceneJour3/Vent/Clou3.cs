fbb251ee48346fc31de67f87538e8607e64c66b4
DevShaD0ow <145599061+DevShaD0ow@users.noreply.github.com>
2025-04-25 17:40:37 -0400

    fin modif j3
    Tout fonctionne (j'ai réduit size du casque)

--- Assets/scriptC#/SceneJour3/Vent/Clou3.cs
+++ Assets/scriptC#/SceneJour3/Vent/Clou3.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Clou3 : MonoBehaviour
 {
     public ClouManager ClouManager;
+    public AudioSource sonClou;
     public int coupsRestants = 3;
     public float enfoncementParCoup = 0.01f;
     public bool estFini = false;
@@ -12,6 +13,7 @@ public class Clou3 : MonoBehaviour
         if (other.CompareTag("marteau") && coupsRestants > 0)
         {
             coupsRestants--;
+            sonClou.Play();
             transform.position -= new Vector3(enfoncementParCoup, 0, 0);
 
             if (coupsRestants == 0 && !estFini)
