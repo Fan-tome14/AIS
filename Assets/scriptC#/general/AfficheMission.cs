6bcbb3819985928707a4247d85f0595d6a89770b
DevShaD0ow <145599061+DevShaD0ow@users.noreply.github.com>
2025-04-28 12:23:30 -0400

    ajout vidéo
    ajout vidéo a la fin en easter egg

--- Assets/scriptC#/general/AfficheMission.cs
+++ Assets/scriptC#/general/AfficheMission.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using System.Collections;
+using UnityEngine.Video;
+
 
 public class AfficheMission : MonoBehaviour
 {
     public GameObject canvas;
     public bool isPressed = false;
+    public CheckMissionsGlobal checkMissionsGlobal; // Référence au script CheckMissionsGlobal
+    public GameObject ecran2;
 
     private UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grabInteractable;
     
@@ -34,7 +38,15 @@ public class AfficheMission : MonoBehaviour
 
     private void OnButtonPressed(SelectEnterEventArgs args)
     {
-        if (isPressed) return; // Si le bouton est déjà pressé, on ne fait rien
+        if (isPressed&&!CheckMissionsGlobal.finishedday) return; // Si le bouton est déjà pressé, on ne fait rien
+        else if (isPressed && CheckMissionsGlobal.finishedday) // Si le bouton est pressé et que la journée est finie, on le remet à sa position initiale
+        {
+            ecran2.SetActive(true); // Affiche l'écran 2
+            var screen = ecran2.GetComponent<VideoPlayer>();
+            if (screen != null) screen.Play(); // Joue la vidéo sur l'écran 2
+            if (canvas != null) canvas.SetActive(false); // Et cache le canvas des missions
+            return;
+        }
         
         isPressed = true; // Sinon marque le bouton comme pressé
         if (canvas != null) canvas.SetActive(true); // Et affiche le canvas des missions
